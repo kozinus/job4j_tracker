4d9f2ea4b2cbb9788b1aea2467be799c0e78d570
kozinus <kkozin2403@gmail.com>
2023-03-19 11:05:00 +0300

    Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,22 +1,23 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
+    private String surname;
 
     private final int sizeOfEmpty = 10;
-    public String surname;
-    public static String name = "1";
+
+    private final String name = "1";
+
+    private static String newValue = "";
+
+    Broken() { }
+
+    public static void echo() { }
 
     public static void method(int a, int b, int c, int d, int e) { }
-
-    void echo() { }
-
-    public static String newValue = "";
 
     public static void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
-
-    Broken() { }
 }
