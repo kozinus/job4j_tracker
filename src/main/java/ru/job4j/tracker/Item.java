e807e72a6721e962e6960f9f835257770376dc9b
kozinus <kkozin2403@gmail.com>
2023-03-24 12:15:36 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -11,10 +11,6 @@ public class Item {
 
     }
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     public Item(String name) {
         this.name = name;
     }
@@ -22,6 +18,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
