390f6b1e574e32d15362783692c91f2507371631
kozinus <kkozin2403@gmail.com>
2023-03-26 03:03:28 +0300

    3.1. Массив с пустыми ячейками [#176470]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,10 +2,9 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book cleanCode = new Book("Clean code", 70);
         Book[] books = new Book[4];
         books[0] = new Book("Война и мир", 800);
-        books[1] = cleanCode;
+        books[1] = new Book("Clean code", 70);
         books[2] = new Book("none", 0);
         books[3] = new Book("Конёк-горбунок", 90);
         for (int i = 0; i < books.length; i++) {
@@ -15,7 +14,7 @@ public class Library {
         books[3] = books[0];
         books[0] = temp;
         for (int i = 0; i < books.length; i++) {
-            if (books[i].equals(cleanCode)) {
+            if (books[i].getTitle().equals("Clean code")) {
                 System.out.println(books[i].getTitle() + " - " + books[i].getPages());
             }
         }
