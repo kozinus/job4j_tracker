ee3a55d845200276f1b0bda9ec522c4d2e45b307
kozinus <kkozin2403@gmail.com>
2023-03-28 14:21:22 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,9 +60,7 @@ public class Tracker {
         if (index == -1) {
             return false;
         }
-        items[index] = null;
-        System.arraycopy(items, index + 1,
-                items, index, size - index - 1);
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
         items[size - 1] = null;
         size--;
         return true;
