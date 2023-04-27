56a9fd1690a19aff1e950e5cf5fc363184b9bebc
kozinus <kkozin2403@gmail.com>
2023-04-28 00:18:29 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("kkozin2403@gmail.com", "Kozin Kirill Yurievich");
+        map.put("kozinus@yahoo.com", "Kozin Kirill Yurievich");
+        map.put("kozinyu@gmail.com", "Kozin Yuriy Yurievich");
+        map.put("kozinus@yahoo.com", "Kozin Kirill Yurievich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
