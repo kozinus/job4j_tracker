3ce4176412e9254fa76b7595266fc3cf6451ec36
kozinus <kkozin2403@gmail.com>
2023-05-02 12:35:54 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -6,16 +6,41 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает работу банковских переводов
+ * @author Kkozi
+ * @version 1.0
+ */
+
 public class BankService {
+    /**
+     * Хранение задания осуществляется в коллекции типа HashMap
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
+
+    /**
+     * Метод добавляет список ArrayList по ключю класса User,
+     * Если такого ключа не найдено
+     * @param user пользователь, который добавляется в качестве ключа
+     */
 
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
+    /**
+     * Метод удаляет пару User - LinkedList<account> по строке passport
+     * @param passport строка для поиска пользователя
+     * @return Возвращает, удалось ли найти и удалить пользователя
+     */
 
     public boolean deleteUser(String passport) {
         return users.remove(new User(passport, "")) != null;
     }
+    /**
+     * Метод находит список по строке и добавляет в него счёт
+     * @param passport строка для поиска пользователя
+     * @param account нужно добавить в список счетов
+     */
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -26,6 +51,10 @@ public class BankService {
             }
         }
     }
+    /**
+     * Метод находит пользователя по строке
+     * @param passport строка для поиска пользователя
+     */
 
     public User findByPassport(String passport) {
         User rsl = null;
@@ -37,6 +66,12 @@ public class BankService {
         }
         return rsl;
     }
+    /**
+     * Метод удаляет пару User - LinkedList<account> по строке passport
+     * @param passport строка для поиска пользователя
+     * @param requisite Строка для поиска счёта у пользователя
+     * @return Возвращает найденный счёт или null
+     */
 
     public Account findByRequisite(String passport, String requisite) {
         User pass = findByPassport(passport);
@@ -49,6 +84,17 @@ public class BankService {
         }
         return null;
     }
+    /**
+     * Метод находит счёт-отправитель и счёт-получатель.
+     * Операция проводится, если найдены отправитель и получатель
+     * и у отправителя хватает средств.
+     * @param srcPassport строка для поиска пользователя-отправителя
+     * @param srcRequisite Строка для поиска счёта-отправителя
+     * @param destPassport строка для поиска пользователя-получателя
+     * @param destRequisite строка для поиска счёта-получателя
+     * @param amount значение перевода
+     * @return успешность перевода
+     */
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
@@ -64,6 +110,11 @@ public class BankService {
         }
         return rsl;
     }
+    /**
+     * Метод находит список счетов по ключу и возвращает его
+     * @param user ключ для получения списка счетов
+     * @return список счетов пользователя
+     */
 
     public List<Account> getAccounts(User user) {
         return users.get(user);
